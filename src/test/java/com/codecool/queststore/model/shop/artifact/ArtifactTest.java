ebb8674fcfca979a0f279cb119913beb10addf43
Seweryn91 <s.talaj@gmail.com>
2018-09-12 13:39:00 +0200

    Refactor ArtifactTest

--- src/test/java/com/codecool/queststore/model/shop/artifact/ArtifactTest.java
+++ src/test/java/com/codecool/queststore/model/shop/artifact/ArtifactTest.java
@@ -1,7 +1,5 @@
 package com.codecool.queststore.model.shop.artifact;
 
-import com.google.common.annotations.VisibleForTesting;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,53 +8,49 @@ class ArtifactTest {
 
 
     @Test
-    void TestIfArtifactAcceptsNegativeCost() {
-        boolean errorFound = false;
-        Artifact artifact = new Artifact(0, 0, "name", "description", -100, "imageName", "imageMarkedName", false);
-        if(artifact.getCOST() < 0){
-            errorFound = true;
-        }
-        Assert.assertFalse(errorFound);
+    public void TestIfArtifactAcceptsNegativeCost() {
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0,"name", "description", 0,"imageMarkedName", "imageMarkedName", null, false));
     }
 
     @Test
     void TestIfArtifactAcceptsEmptyName(){
-        boolean errorFound = true;
-        Artifact artifact = new Artifact(0, 0, "", "description", 0, "imageName", "imageMarkedName", false);
-        if(!artifact.getNAME().equals("")){
-            errorFound = false;
-        }
-        Assert.assertFalse(errorFound);
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0, 0, "", "description", 0, "imageName", "imageMarkedName", null, false));
+    }
+
+    @Test
+    void TestIfArtifactAcceptsNullName(){
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0, 0, null, "description", 0, "imageName", "imageMarkedName", null, false));
     }
 
     @Test
     void TestIfArtifactAcceptsEmptyDescription(){
-        boolean errorFound = true;
-        Artifact artifact = new Artifact(0, 0, "name", "", 0, "imageName", "imageMarkedName", false);
-        if(!artifact.getDESCRIPTION().equals("")){
-            errorFound = false;
-        }
-        Assert.assertFalse(errorFound);
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0, "name", "description",0,"imageName","imageName",null, false));
     }
 
     @Test
+    void TestIfArtifactAcceptsNullDescription(){
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0, 0, "name", null, 0, "imageName", "imageMarkedName", null, false));
+    }
+
+
+    @Test
     void TestIfArtifactAcceptsEmptyImageName(){
-        boolean errorFound = true;
-        Artifact artifact = new Artifact(0, 0, "name", "description", 0, "", "imageMarkedName", false);
-        if(!artifact.getIMAGE_FILENAME().equals("")){
-            errorFound = false;
-        }
-        Assert.assertFalse(errorFound);
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0,"name","description",0,"","imageMarkedName", null,false));
+    }
+
+    @Test
+    void TestIfArtifactAcceptsNullImageName(){
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0,"name","description",0,null,"imageMarkedName", null,false));
     }
 
     @Test
     void TestIfArtifactAcceptsEmptyImageMarkedName(){
-        boolean errorFound = true;
-        Artifact artifact = new Artifact(0, 0, "name", "description", 0, "imageName", "", false);
-        if(!artifact.getIMAGE_MARKED_FILENAME().equals("")){
-            errorFound = false;
-        }
-        Assert.assertFalse(errorFound);
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0,"name","description",0,"imageName","", null, false));
+    }
+
+    @Test
+    void TestIfArtifactAcceptsNullImageMarkedName(){
+        assertThrows(IllegalArgumentException.class, () -> new Artifact(0,0,"name","description",0,"imageName",null, null, false));
     }
 
 }
