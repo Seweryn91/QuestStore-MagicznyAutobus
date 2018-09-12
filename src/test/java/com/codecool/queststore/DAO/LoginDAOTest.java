cdd0630db840faa2ace4fdba61e301fa20a9ec0f
polon59 <przemyslaw.polczak@gmail.com>
2018-09-12 14:16:33 +0200

    Implement test method for 'validate()'

---
+++ src/test/java/com/codecool/queststore/DAO/LoginDAOTest.java
@@ -0,0 +1,30 @@
+package com.codecool.queststore.DAO;
+
+import com.codecool.queststore.model.Login;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class LoginDAOTest {
+    LoginDAO logDAO = new LoginDAO();
+
+    @Mock
+    private Login login;
+
+    @Test
+    void testValidation() throws SQLException {
+        MockitoAnnotations.initMocks(this);
+        when(login.getLOGIN()).thenReturn("admin");
+        when(login.getPASSWORD()).thenReturn("123");
+
+        int result = logDAO.validation(login);
+        assertEquals(1,result);
+    }
+
+
+}
