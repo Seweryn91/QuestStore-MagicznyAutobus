1473b86206a902fe81e8f5acffd594322a7e8a01
polon59 <przemyslaw.polczak@gmail.com>
2018-09-13 12:28:03 +0200

    Remove test method 'createMentorTest()'

--- src/test/java/com/codecool/queststore/DAO/MentorDAOTest.java
+++ src/test/java/com/codecool/queststore/DAO/MentorDAOTest.java
@@ -3,29 +3,18 @@ package com.codecool.queststore.DAO;
 import com.codecool.queststore.model.Login;
 import com.codecool.queststore.model.user.Role;
 import com.codecool.queststore.model.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class MentorDAOTest {
 
     private MentorDAO mentorDAO = new MentorDAO();
     private User user = new User("testname","testsurname","testemail","testadress",10, Role.MENTOR);
     private Login login = new Login("testlogin","testpassword");
-
-
-    @Test
-    void createMentorTest(){
-        boolean created = false;
-
-        try{
-            created = mentorDAO.createMentor(user,login);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        assertTrue(created);
-    }
 
 
     @Test
