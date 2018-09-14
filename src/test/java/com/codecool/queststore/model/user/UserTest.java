f33f5e7b55a075491a240bf0ba5110a4ed0bcc22
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-09-14 09:27:29 +0200

    Tests for class User, tests don't pass

--- src/test/java/com/codecool/queststore/model/user/UserTest.java
+++ src/test/java/com/codecool/queststore/model/user/UserTest.java
@@ -1,5 +1,36 @@
+package com.codecool.queststore.model.user;
+
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserTest {
+public class UserTest {
+
+    @Test
+    public void testIsNameIsNotNull() {
+        User user = new User("Adam", "Kowalski", "test@wp.pl", "Katowice", 1, Role.MENTOR);
+        assertNotNull(user.getNAME());
+    }
+
+    @Test
+    public void testWrongEmailIllegalArgumentsThrowsException() {
+        assertThrows(IllegalArgumentException.class, ()-> {
+            new User("Ola", "Kowalski", "www", "Katowice", 1, Role.MENTOR);
+        });
+    }
+
+    @Test
+    public void testWrongIDIllegalArgumentsThrowsException() {
+        assertThrows(IllegalArgumentException.class, ()-> {
+            new User("Ola", "Kowalski", "test@wp.pl", "Katowice", -11, Role.MENTOR);
+        });
+    }
+
+    @Test
+    public void testNullNameIllegalArgumentsThrowsException() {
+        assertThrows(IllegalArgumentException.class, ()-> {
+            new User(null, "Kowalski", "www", "Katowice", 1, Role.MENTOR);
+        });
+    }
 
 }
